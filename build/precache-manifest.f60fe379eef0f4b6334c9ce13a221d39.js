5eb6f7097c4fbb9bfcc736c8115deb3e80b43bd8
kartik <marshal@apptunix.com>
2019-09-03 10:44:51 +0530

    First Commit

---
+++ build/precache-manifest.f60fe379eef0f4b6334c9ce13a221d39.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da4fdb2583a73216e6ec994d0fb3874d",
+    "url": "/canyou_admin/index.html"
+  },
+  {
+    "revision": "d138249cadd6985f269a",
+    "url": "/canyou_admin/static/css/2.165c6c9c.chunk.css"
+  },
+  {
+    "revision": "98726216e1e9e2d89774",
+    "url": "/canyou_admin/static/css/main.af66404f.chunk.css"
+  },
+  {
+    "revision": "d138249cadd6985f269a",
+    "url": "/canyou_admin/static/js/2.d3ae5640.chunk.js"
+  },
+  {
+    "revision": "98726216e1e9e2d89774",
+    "url": "/canyou_admin/static/js/main.35e5f1a0.chunk.js"
+  },
+  {
+    "revision": "548e919a995bb6c976ad",
+    "url": "/canyou_admin/static/js/runtime~main.bc212ad4.js"
+  }
+]);
